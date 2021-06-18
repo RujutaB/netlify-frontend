f6cc97ea788b6c51e0adf9d6676e4d208554956a
RujutaB <60830306+RujutaB@users.noreply.github.com>
2021-06-18 13:16:12 +0530

    Add files via upload

---
+++ src/components/Product.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Card } from 'react-bootstrap'
+import Rating from './Rating'
+import {Link} from 'react-router-dom'
+
+const Product = ({product}) => {   //destructuring product array instead of props
+    return (
+        <Card className='my-3 p-3 rounded'>  {/*add margin and padding*/}
+            <Link to= {`/product/${product.id}`}>
+                <Card.Img src = {product.image} variant='top' /> {/*photo*/}
+            </Link>
+            <Card.Body>
+                <Link to= {`/product/${product.id}`}>    {/*title*/}
+                    <Card.Title as ='div'>
+                        <strong>{product.name}</strong>
+                    </Card.Title>
+                </Link> 
+                
+                <Card.Text as ='div'> {/*review*/}
+                    <Rating  
+                    value ={product.rating}
+                    text ={`${product.numReviews} reviews`}
+                    /*here we could have set color-prop to something, instead we set its default value in Rating.js itself*/
+                    />
+                </Card.Text>  
+
+                <Card.Text as ='h6'> {/*price*/}
+                    ${product.price}    
+                </Card.Text> 
+            </Card.Body>
+        </Card>
+    )
+}
+
+export default Product
